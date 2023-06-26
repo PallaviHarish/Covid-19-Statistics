66cdcfa7070e602e64689c57aabc6193decf9b8c
Pallavi Harish <106737458+PallaviHarish@users.noreply.github.com>
2023-06-26 17:27:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
+function findStat() {      
+    var c=document.getElementById("country").value;
+    const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '1d0ae464cfmsh6b3d460e2d54394p14f578jsnbb180000e711',
+        'X-RapidAPI-Host': 'covid-19-coronavirus-statistics.p.rapidapi.com'
+    }
+    };
 
-        function calc(){
-            var a=parseInt(document.getElementById("math").value);
-            var b=parseInt(document.getElementById("ds").value);
-            var c=parseInt(document.getElementById("ade").value);
-            var d=parseInt(document.getElementById("co").value);
-            var e=parseInt(document.getElementById("java").value);
-            var f=parseInt(document.getElementById("c++").value);
-            var g=parseInt(document.getElementById("kan").value);
-            var h=parseInt(document.getElementById("uh").value);
-
-            var a1=3*marks(a);
-            var b1=4*marks(b);
-            var c1=4*marks(c);
-            var d1=3*marks(d);
-            var e1=1*marks(e);
-            var f1=1*marks(f);
-            var g1=1*marks(g);
-            var h1=1*marks(h);
-
-            var z=(a1+b1+c1+d1+e1+f1+g1+h1)/18;
-            var result=z.toFixed(2);
-            document.getElementById("res").value=parseFloat(result);
-        }
-        function marks(y)
-        {
-            if(y>=90)
-            return 10;
-            else if (y<=89&&y>=80)
-            return 9;
-            else if (y<=79&&y>=70)
-            return 8;
-            else if (y<=69&&y>=60)
-            return 7;
-            else if (y<=59&&y>=50)
-            return 6;
-            else if (y<=49&&y>=45)
-            return 5;
-            else if (y<=44&&y>=40)
-            return 4;
-            else
-            return 0; 
-        }
+    fetch('https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total?country='+c, options)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        document.getElementById('confirmed').innerHTML="Confirmed Cases: "+response.data.confirmed;
+        document.getElementById('deaths').innerHTML="Deaths: "+response.data.deaths;
+        document.getElementById('location').innerHTML="Location: "+response.data.location;
+        document.getElementById('lastChecked').innerHTML="Last Updated On: "+response.data.lastChecked;
+    })
+    .catch(err => console.error(err));
+}
